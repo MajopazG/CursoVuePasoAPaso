5c416f89168fd7d5a6ce4025eb948d849e8ed1d9
Maria Jose Paz Garcia <majopazgarcia25@gmail.com>
2023-03-21 15:27:43 -0500

    finalising the components-1

--- components-1/src/App.vue
+++ components-1/src/App.vue
@@ -3,8 +3,13 @@ import { ref } from "vue";
 
 import BlogPost from './components/BlogPost.vue';
 import PaginatePots from './components/PaginatePots.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
 
 const posts = ref([]);
+const postXpage = 10;
+const inicio = ref(0);
+const fin = ref(postXpage);
+const loading = ref(true);
 
 const miFavorito = ref("");
 
@@ -12,23 +17,37 @@ const fijarFavorito = (title) => {
   miFavorito.value = title; 
 };
 
+const next = () => {
+    inicio.value = inicio.value + postXpage
+    fin.value = fin.value + postXpage
+};
+
+const previus =() => {
+    inicio.value = inicio.value - postXpage
+    fin.value = fin.value -postXpage
+    // inicio.value += - postXpage
+    // fin.value += -postXpage
+};
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then((res) => res.json())
-    .then((data) => posts.value = data);
+    .then((data) => posts.value = data)
+    .finally(() => loading.value = false);
 
 </script>
 
 <template>
-  <div class="container">
+  <LoadingSpinner v-if="loading"/>
+  <div class="container" v-else>
     <h1>APP</h1>
     <h2>Mis Post Favoritos:
       {{ miFavorito || "Sin Favorito" }}
     </h2>
 
-    <PaginatePots class="mb-2"/>
+    <PaginatePots @next="next" @previus="previus" :inicio="inicio" :fin="fin" :maxLength="posts.length" class="mb-2"/>
 
     <BlogPost 
-      v-for="post in posts"
+      v-for="post in posts.slice(inicio, fin)"
       :key="post.id"
       :id="post.id"
       :title="post.title"
--- components-1/src/components/PaginatePots.vue
+++ components-1/src/components/PaginatePots.vue
@@ -1,9 +1,28 @@
 <script setup>
+
+defineProps(['inicio', 'fin', 'maxLength']);
+
+const emit = defineEmits(['next', 'previus']);
+
 </script>
 
 <template>
     <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-primary">Previus</button>
-        <button type="button" class="btn btn-primary">Next</button>
+        <button 
+            type="button" 
+            class="btn btn-primary" 
+            @click="emit('previus')"
+            :disabled="inicio <= 0"
+        >
+            Previus {{ inicio }}
+        </button>
+        <button 
+            type="button" 
+            class="btn btn-primary" 
+            @click="emit('next')"
+            :disabled="fin >=maxLength"
+        >
+            Next {{ fin }}
+        </button>
     </div>
 </template>
